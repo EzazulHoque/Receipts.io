f3283914796c67bde8ca71966284f0b9bd2699a3
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-03-05 21:26:40 +0800

    added firebase config file

--- receiptsio/src/App.js
+++ receiptsio/src/App.js
@@ -1,84 +1,117 @@
-
-import React,  { useState } from 'react'
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
-import './App.css';
-import fon from './pics/fon.png'
+import React, { useState } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "./App.css";
+import fon from "./pics/fon.png";
 
 function App() {
   const [show, setShow] = useState(false);
-  const showDropdown = (e)=>{
-      setShow(!show);
-  }
-  const hideDropdown = e => {
-      setShow(false);
-  }
+  const showDropdown = (e) => {
+    setShow(!show);
+  };
+  const hideDropdown = (e) => {
+    setShow(false);
+  };
   return (
     <div className="App">
       <div class="nav-container">
-      <Navbar collapseOnSelect expand="xl"  variant="light"  style={{padding:"10px"}}>
-        <Navbar.Brand style={{fontSize:"30px"}} href="#home">Receipts.io</Navbar.Brand>
+        <Navbar
+          collapseOnSelect
+          expand="xl"
+          variant="light"
+          style={{ padding: "10px" }}
+        >
+          <Navbar.Brand style={{ fontSize: "30px" }} href="#home">
+            Receipts.io
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto" style={{fontSize:"20px"}} >
-                <NavDropdown 
-                title="Reciepts" 
-                id="dropdown-basic" 
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" style={{ fontSize: "20px" }}>
+              <NavDropdown
+                title="Reciepts"
+                id="dropdown-basic"
                 show={show}
-                onMouseEnter={showDropdown} 
-                onMouseLeave={hideDropdown} >
+                onMouseEnter={showDropdown}
+                onMouseLeave={hideDropdown}
+              >
+                <div style={{ width: "1000px" }}>
+                  <div class="row">
+                    <div class="col">
+                      <NavDropdown.Item style={{ borderRadius: "10px" }}>
+                        <h2>Simple</h2>
+                        <p>
+                          Simple and timeless, with a traditional structured
+                          layout. <br /> A perfect artistic look.
+                        </p>
+                      </NavDropdown.Item>
+                      <NavDropdown.Item style={{ borderRadius: "10px" }}>
+                        {" "}
+                        <h2>Professional</h2>
+                        <p>
+                          The finest designs for displaying professionalism,{" "}
+                          <br /> dependability and proficiency.
+                        </p>
+                      </NavDropdown.Item>
+                    </div>
 
-        <div style={{width:"1000px"}}>
-          <div class="row">
-            <div class="col">
-              <NavDropdown.Item style={{borderRadius:"10px"}}>
-                <h2>Simple</h2>
-                <p>Simple and timeless, with a traditional structured layout. <br/> A perfect artistic look.</p>
-              </NavDropdown.Item>
-                <NavDropdown.Item style={{borderRadius:"10px"}}> <h2>Professional</h2>
-                  <p>The finest designs for displaying professionalism, <br/> dependability and proficiency.</p></NavDropdown.Item>
-            </div>
-
-            <div class="col">
-              <NavDropdown.Item style={{borderRadius:"10px"}}> <h2>Creative</h2>
-                <p>A exclusive look that is ideal for creative businesses <br/> and extant companies.</p></NavDropdown.Item>
-                  {/* <NavDropdown.Divider /> */}
-                  <NavDropdown.Item style={{borderRadius:"10px"}}> <h2>Modern</h2>
-                <p>The new and trendy feeling of the forward-thinking <br/> businesses in innovative fields</p></NavDropdown.Item>
-            </div>
-          </div>
+                    <div class="col">
+                      <NavDropdown.Item style={{ borderRadius: "10px" }}>
+                        {" "}
+                        <h2>Creative</h2>
+                        <p>
+                          A exclusive look that is ideal for creative businesses{" "}
+                          <br /> and extant companies.
+                        </p>
+                      </NavDropdown.Item>
+                      {/* <NavDropdown.Divider /> */}
+                      <NavDropdown.Item style={{ borderRadius: "10px" }}>
+                        {" "}
+                        <h2>Modern</h2>
+                        <p>
+                          The new and trendy feeling of the forward-thinking{" "}
+                          <br /> businesses in innovative fields
+                        </p>
+                      </NavDropdown.Item>
+                    </div>
+                  </div>
                 </div>
-      </NavDropdown>
-      
-      <Nav.Link href="/features">Features</Nav.Link>
-      <Nav.Link href="/pricing">Pricing</Nav.Link>
-      <Nav.Link href="/pricing">API</Nav.Link>
-      <Nav.Link href="/demo">Demo</Nav.Link>
-      <Nav.Link href="/aboutus">About Us</Nav.Link>
-    </Nav>
-    <Nav style={{fontSize:"20px"}}>
-      {/* <Nav.Link eventKey={2} href="/Login">
+              </NavDropdown>
+
+              <Nav.Link href="/features">Features</Nav.Link>
+              <Nav.Link href="/pricing">Pricing</Nav.Link>
+              <Nav.Link href="/pricing">API</Nav.Link>
+              <Nav.Link href="/demo">Demo</Nav.Link>
+              <Nav.Link href="/aboutus">About Us</Nav.Link>
+            </Nav>
+            <Nav style={{ fontSize: "20px" }}>
+              {/* <Nav.Link eventKey={2} href="/Login">
         Login
       </Nav.Link> */}
-      <Nav.Link style={{color:"black"}} eventKey="3" href="/Login">
-       <button class="btn btn-outline-success btn-lg">Login</button>
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+              <Nav.Link style={{ color: "black" }} eventKey="3" href="/Login">
+                <button class="btn btn-outline-success btn-lg">Login</button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
       {/* End of Nav Container */}
-      
+
       <div class="landing">
         <div class="container">
           <div class="pad-10">
             <div align="center" class="h1 mt-5">
-            Transform your local business into a more <strong style={{color:"green"}}>greener environment</strong> by using digital receipts.
+              Transform your local business into a more{" "}
+              <strong style={{ color: "green" }}>greener environment</strong> by
+              using digital receipts.
             </div>
             <div align="center" class="h4 mt-5">
-            Use professional receipt templates that follow the exact ‘traditional receipt structure’. Easy to use and done within minutes - try now for free!
+              Use professional receipt templates that follow the exact
+              ‘traditional receipt structure’. Easy to use and done within
+              minutes - try now for free!
             </div>
             <div align="center" class="mt-5">
-            <button type="button" class="btn btn-success btn-lg">Create Receipt</button>
+              <button type="button" class="btn btn-success btn-lg">
+                Create Receipt
+              </button>
             </div>
           </div>
         </div>
@@ -88,20 +121,32 @@ function App() {
       <div class="secondSection">
         <div class="container">
           <div class="row">
-          {/* Left */}
+            {/* Left */}
             <div class="col">
               <div class="h1">
-              Contribute to a <strong style={{color:"green"}}>greener environment</strong> by getting rid of <strong style={{color:"red"}}>paper reciepts</strong>
+                Contribute to a{" "}
+                <strong style={{ color: "green" }}>greener environment</strong>{" "}
+                by getting rid of{" "}
+                <strong style={{ color: "red" }}>paper reciepts</strong>
               </div>
-                <div class="mt-5">
-                  <div class="h3">Paper reciepts contribute to 30% of the world's pollution.</div>
-                  <div class="h3 mt-3">3 million trees and 600,000 gallons of oil are used to make paper reciepts every year!</div>
-                  <div class="h3 mt-3">In this new era of financial technology, it’s essential for businesses to take advantage of this technology and move to a greener approach.</div>
+              <div class="mt-5">
+                <div class="h3">
+                  Paper reciepts contribute to 30% of the world's pollution.
                 </div>
+                <div class="h3 mt-3">
+                  3 million trees and 600,000 gallons of oil are used to make
+                  paper reciepts every year!
+                </div>
+                <div class="h3 mt-3">
+                  In this new era of financial technology, it’s essential for
+                  businesses to take advantage of this technology and move to a
+                  greener approach.
+                </div>
+              </div>
             </div>
             {/* Right */}
             <div class="col">
-            <img src={fon} />
+              <img src={fon} />
             </div>
           </div>
         </div>
@@ -110,11 +155,11 @@ function App() {
 
       <div class="thirdSection">
         <div class="container">
-          <div align="center" class="h1">How does Receipts.io work?</div>
-            
+          <div align="center" class="h1">
+            How does Receipts.io work?
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
---
+++ receiptsio/src/firebase/config.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkwp7aA3kkGluX8cRThNiXXFO56_wObTw",
+  authDomain: "receiptsio.firebaseapp.com",
+  projectId: "receiptsio",
+  storageBucket: "receiptsio.appspot.com",
+  messagingSenderId: "951733286985",
+  appId: "1:951733286985:web:b07719ed6c38a00a66a798",
+  measurementId: "G-7JHWW73RMT",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
