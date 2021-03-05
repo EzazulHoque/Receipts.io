d326a74d1fca958c0d09f5a592df6d347aef29e9
Ezazul Hoque <niloyjademathers@gmail.com>
2021-03-06 00:15:15 +0600

    Done with the Pricing Page.

--- receiptsio/src/pages/Pricing.js
+++ receiptsio/src/pages/Pricing.js
@@ -1,5 +1,232 @@
 import React from "react";
 
-const Pricing = () => <div>Hello from Pricing</div>;
+const Pricing = () => 
+<div>
+    <div class="container-fluid">
+        <div class="pad-5">
+            <div align="center" class="h1">
+            Pricing Plans
+            </div>
+            <div align="center" class="h5 mt-5">
+            Try our digital solution for for free, then choose to migrate to a plan that fits you.
+            </div>
+            <div class="mt-5">
+                <div class="row mt-5 pad-5">
+                    <div class="col" align="center">
+                        <div class="pricing pad-5">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Free
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    0$
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Basic Features
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    Generate up to 1000 custom receipts
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    Customers can scan QR code directly from your POS software
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    3 months
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    *Developer Support
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row" style={{marginTop:"30%"}}>
+                                <div class="col">
+                                    <div class="mt-3">
+                                        <button class="btn btn-outline-dark btn-lg">Get Started</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Second Column */}
+                    <div class="col" align="center">
+                    <div class="pricing pad-5">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Rocket
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    80$
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Premium Features
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-4">
+                                    Unlimited custom receipts
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-4">
+                                    Customers can scan QR code directly from your POS software
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    6 months
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    Customer data analytics for 500 customers.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    *Developer Support
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mt-5">
+                                        <button class="btn btn-outline-dark btn-lg">Get Started</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col" align="center">
+                    <div class="pricing pad-5">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Spaceship
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    180$
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h3 mt-2">
+                                    Premium Features
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-4">
+                                    Unlimited custom receipts
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-4">
+                                    Customers can scan QR code directly from your POS software
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    12 months
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    Customer data analytics for 1000 customers.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="h5 mt-3">
+                                    *Developer Support
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mt-5">
+                                        <button class="btn btn-outline-dark btn-lg">Get Started</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <div class="container pad-10">
+        <div class="h1">
+        Hello123
+        </div>
+    </div> */}
+    </div>
+    {/* End of Container JESUS CHRIST! IT LOOKS LIKE A FUCKING STAIRCASE!!! */}
+   <div style={{marginTop:"30%"}}>
+   <div class="container-fluid mt-5 pad-10">
+        <div class="h1 mt-5">
+        Dedicated Solutions
+        </div>
+        <div class="h2 mt-3">
+        For enterprise clients and POS Software with special requirements, we can run a dedicated Receipts.io instance on a cluster in the Google Cloud Platform region of your choice. Contact us to learn more.
+        </div>
+    </div>
+   </div>
+</div>;
 
 export default Pricing;
