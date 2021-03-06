20d0238c457c2272ea6989d248b8003a924887b4
Ezazul Hoque <niloyjademathers@gmail.com>
2021-03-06 23:33:36 +0600

    Added two additonal designs

--- receiptsio/src/components/mobileApp/Receipt copy 2.js
+++ receiptsio/src/components/mobileApp/Receipt copy 2.js
@@ -2,46 +2,51 @@ import React from "react";
 
 const Receipt = ({ receipt, setSelectedReceipt, items }) => {
   return <div>
-  {/* Bakery Shop */}
+  {/* General Store */}
   <div class="bigRcp mt-5">
   {/* Top Header */}
-
-    {/* <div class="row">
+    <div class="h1 mt-2">
+      Springvale General Store
+    </div>
+    <div class="row">
       <div class="col" style={{marginLeft:"6%"}}>
       POS: 84561
       </div>
       <div class="col" style={{marginRight:"10%"}}>
         {receipt.date}
       </div>
-    </div> */}
-    {/* TopBar */}
-    <div class="genTopB mt-4">
-    <div class="h4 mt-4">
-      Khin & Kim Bakery
     </div>
+    {/* TopBar */}
+    <div class="genTop">
+      <div class="h1 mt-1">
+        $21.50
+      </div>
     </div>
     {/* Mid Section */}
-    <div class="row mt-5">
-    {/* <div style={{marginLeft:"5%"}} class="line"></div> */}
+    <div class="row mt-4">
+    <div style={{marginLeft:"5%"}} class="line"></div>
       <div class="col mt-2">
-          <div style={{marginLeft:"3%"}} class="h6 mt-3">Almond croissant</div>
-          <div style={{marginLeft:"2%"}} class="h6 mt-4">Choclate eclair
-          </div>
-          <div style={{marginLeft:"2%"}} class="h6 mt-4">Long Machiato</div>
+        <div align="center" class="h3" style={{marginLeft:"5%"}}>
+          Description
+        </div>
+          <div style={{marginLeft:"2%"}} class="h6 mt-3">Banana Canvendish 1.177 kg</div>
+          <div style={{marginLeft:"2%"}} class="h6 mt-4">Tomato Gourmet Loose 0.428kg</div>
+          <div style={{marginLeft:"2%"}} class="h6 mt-4">Melon Honeydew</div>
 
       </div>
       <div class="col mt-2">
-      {/* <div class="h3">
+      <div class="h3">
           Amount
-        </div> */}
-        <div style={{marginLeft:"0%"}} class="h6 mt-4">$ 4.12</div>
-        <div style={{marginLeft:"2%"}} class="h6 mt-3">$ 3.90</div>
+        </div>
+        <div style={{marginLeft:"2%"}} class="h6 mt-4">$ 4.12</div>
+        <div style={{marginLeft:"2%"}} class="h6 mt-5">$ 3.90</div>
         <div style={{marginLeft:"2%"}} class="h6 mt-4">$ 3.00</div>
 
       </div>
-            </div>
+      <div style={{marginLeft:"5%"}} class="line"></div>
+      </div>
       {/* Total Section */}
-      <div class="row mt-5">
+      <div class="row mt-4">
             <div class="col">
               <div class="h3">Total</div>
             </div>
@@ -49,8 +54,6 @@ const Receipt = ({ receipt, setSelectedReceipt, items }) => {
               <div class="h3">$11.02</div>
             </div>
           </div>
-          <div  class="line"></div>
-
   </div>
   <div class="mt-5">
     <button
