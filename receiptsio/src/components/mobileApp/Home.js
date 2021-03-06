f2f5ec1f2412a889fa236a51fdc095ae5e2fe7f3
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-03-06 15:43:19 +0800

    added receipt view for mobile app

--- receiptsio/src/components/mobileApp/Home.js
+++ receiptsio/src/components/mobileApp/Home.js
@@ -1,11 +1,33 @@
 import React from "react";
+import ReceiptPreview from "./ReceiptPreview";
 import end from "../../pics/end.png";
 import wl from "../../pics/wl.png";
 import bc from "../../pics/bc.png";
 import ds from "../../pics/ds.png";
 
+const brunetti = {
+  logo: bc,
+  businessName: "Brunetti Carlton",
+  date: "05 Mar 2021",
+  transactionId: "B106",
+  total: 14.21,
+};
 
+const woolworths = {
+  logo: wl,
+  businessName: "Woolworths",
+  date: "04 Mar 2021",
+  transactionId: "83456",
+  total: 21.52,
+};
 
+const daiso = {
+  logo: ds,
+  businessName: "Daiso Japan",
+  date: "03 Mar 2021",
+  transactionId: "3049683456",
+  total: 14,
+};
 
 const Home = () => (
   <div>
@@ -24,54 +46,9 @@ const Home = () => (
       {/* This is where users will be able to scroll through receipts */}
 
       {/* Need another routung for the smaller receipts */}
-        <div class="smlRcp mt-4">
-          <div class="row mt-3">
-            <div class="col">
-              <img src={bc} style={{width:"92px", marginLeft:"20%", marginTop:"25%", borderRadius:"20px"}} />
-            </div>
-            <div class="col" style={{float:"left"}}> 
-              <div class="h6">Brunetti Carlton</div>
-              <div class="h6">05 Mar 2021</div>
-              <div class="h6">B106</div>
-
-            </div>
-            <div class="col">
-              <div class="mon h5" style={{marginTop:"30%"}}>$14.21</div>
-            </div>
-          </div>
-        </div>
-        <div class="smlRcp mt-4">
-          <div class="row mt-3">
-            <div class="col">
-              <img src={wl} style={{width:"92px", marginLeft:"20%", marginTop:"5%", borderRadius:"20px"}} />
-            </div>
-            <div class="col" style={{float:"left"}}> 
-              <div class="h6">Woolsworth</div>
-              <div class="h6">04 Mar 2021</div>
-              <div class="h6">83456</div>
-
-            </div>
-            <div class="col">
-              <div class="mon h5" style={{marginTop:"30%"}}>$21.52</div>
-            </div>
-          </div>
-        </div>
-        <div class="smlRcp mt-4 mb-3">
-          <div class="row mt-3">
-            <div class="col">
-              <img src={ds} style={{width:"92px", marginLeft:"20%", marginTop:"25%", borderRadius:"20px"}} />
-            </div>
-            <div class="col" style={{float:"left"}}> 
-              <div class="h6">Daiso Japan</div>
-              <div class="h6">03 Mar 2021</div>
-              <div class="h6">3049683456</div>
-
-            </div>
-            <div class="col">
-              <div class="mon h5" style={{marginTop:"30%"}}>$14</div>
-            </div>
-          </div>
-        </div>
+      <ReceiptPreview receipt={brunetti} />
+      <ReceiptPreview receipt={woolworths} />
+      <ReceiptPreview receipt={daiso} />
     </div>
   </div>
 );
---
+++ receiptsio/src/components/mobileApp/ReceiptPreview/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./ReceiptPreview.css";
+
+const ReceiptPreview = ({ receipt }) => {
+  return (
+    <Link
+      to={"/mobileApp/receipt/" + receipt.transactionId}
+      className="receipt-preview"
+    >
+      <div class="smlRcp mt-4">
+        <div class="row mt-3">
+          <div class="col">
+            <img
+              src={receipt.logo}
+              style={{
+                width: "92px",
+                marginLeft: "20%",
+                marginTop: "25%",
+                borderRadius: "20px",
+              }}
+            />
+          </div>
+          <div class="col" style={{ float: "left" }}>
+            <div class="h6">{receipt.businessName}</div>
+            <div class="h6">{receipt.date}</div>
+            <div class="h6">{receipt.transactionId}</div>
+          </div>
+          <div class="col">
+            <div class="mon h5" style={{ marginTop: "30%" }}>
+              ${receipt.total}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ReceiptPreview;
