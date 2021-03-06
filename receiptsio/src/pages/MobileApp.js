94e8d2b857b2722b9d4a19875bf4e608601e84bf
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-03-06 13:01:57 +0800

    fixed mobile app basic layout

--- receiptsio/src/pages/MobileApp.js
+++ receiptsio/src/pages/MobileApp.js
@@ -12,7 +12,6 @@ import ReceiptsList from "../components/mobileApp/ReceiptsList";
 import Rewards from "../components/mobileApp/Rewards";
 import Home from "../components/mobileApp/Home";
 
-import end from "../pics/end.png";
 import hom from "../pics/homeButton.png";
 import rcp from "../pics/receiptButton.png";
 import rwd from "../pics/rewards.png";
@@ -33,37 +32,20 @@ const MobileApp = () => {
         </div>
         <div align="center">
           <div class="iPhone">
-            <div class="topBar mt-5">
-              <div class="h2 mt-5" style={{ float: "left", marginLeft: "5%" }}>
-                Hello Ezaz!
-              </div>
-              <div
-                class="circle mt-4"
-                style={{ float: "right", marginRight: "5%" }}
-              >
-                <img src={end} style={{ width: "79px" }} />
-              </div>
-            </div>
-            <div class="h1 mt-5" style={{ marginRight: "35%" }}>
-              Dashboard
-            </div>
-            <div class="dash">
-              {/* This is where users will be able to scroll through receipts */}
-              <Switch>
-                <Route path={`${match.path}/receiptsList`}>
-                  <ReceiptsList />
-                </Route>
-                <Route path={`${match.path}/rewards`}>
-                  <Rewards />
-                </Route>
-                <Route path={`${match.path}/accounts`}>
-                  <Accounts />
-                </Route>
-                <Route path={`${match.path}`}>
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
+            <Switch>
+              <Route path={`${match.path}/receiptsList`}>
+                <ReceiptsList />
+              </Route>
+              <Route path={`${match.path}/rewards`}>
+                <Rewards />
+              </Route>
+              <Route path={`${match.path}/accounts`}>
+                <Accounts />
+              </Route>
+              <Route path={`${match.path}`}>
+                <Home />
+              </Route>
+            </Switch>
             <div class="botNav">
               <div class="row">
                 <div class="col">
